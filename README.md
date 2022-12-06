5833a22f58481be30a276f92d258c03a1cd04dee
Find-NICK <116571277+Find-NICK@users.noreply.github.com>
2022-12-06 11:38:16 +0800

    UPDATE

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 ## The modern TUI downloader,Build with C++ and LOVE ##
 ------
 ### HOW TO USE ###
-1. copy the NEWEST ` aria2 ` to the ` .\aria2 ` folder
-2. run ` IDOWN.exe `
+1. copy the NEWEST [aria](https://github.com/aria2/aria2/releases) to the ` .\aria2 ` folder (if you want to use the newest version)
+2.complie and run ` IDOWN.exe `
 
 
 ---
-* ## License ## 
+ ## License ##
 IDOWN use SATA License (Star And Thank Author License), so you should star this project before using. Read the license carefully.
--- idown.cpp
+++ idown.cpp
@@ -1,27 +1,85 @@
 #include<cstdio>
 #include<iostream>
 #include<windows.h>
-#include<string> 
+#include<string>
 using namespace std;
-string URI,strf,DIR,USEROPT,Personset;
-int connect=32;
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+#define MAXLEN 1000
+string URI,strf=".\\aria2\\aria2c ",DIR,USEROPT,Personset;
+int connection=32;
 char f[5000];
 void downinit();
 void down();
-void settings;
+void settings();
 void init();
+void setcolor(WORD colors) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colors);
+}
+void gotoxy(short x, short y) {
+    COORD coord = {x, y}; 
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
 string int2str(int x);
-void downinit(){
-	printf("©°©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©´\n©¦                                       ©¦\n©¦  Paste your URL here,and PRESS ENTER  ©¦\n©¦                                       ©¦\n©¸©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¼\n");
+int list(char head[],char choice[][MAXLEN],int nums) {
+	int choose=1;
+	printf("%s\n",head);
+	for(int i=0; i<nums; i++) {
+		if(i==choose) {
+			setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|BACKGROUND_BLUE|BACKGROUND_GREEN);
+		} else {
+			setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+		}
+		printf("%s\n",choice[i]);
+	}
+	while(!KEY_DOWN(VK_RETURN)) {
+		if(KEY_DOWN(VK_UP) || KEY_DOWN(87)) {
+			setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+			gotoxy(0,0);
+			choose=max(choose-1,0);
+			printf("%s\n",head);
+			for(int i=0; i<nums; i++) {
+				if(i==choose) {
+					setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|BACKGROUND_BLUE|BACKGROUND_GREEN);
+				} else {
+					setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+				}
+				printf("%s\n",choice[i]);
+			}
+			Sleep(50);
+		}
+		if(KEY_DOWN(VK_DOWN)||KEY_DOWN(83)) {
+			setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+			gotoxy(0,0);
+			choose=min(choose+1,nums-1);
+			printf("%s\n",head);
+			for(int i=0; i<nums; i++) {
+				if(i==choose) {
+					setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|BACKGROUND_BLUE|BACKGROUND_GREEN);
+				} else {
+					setcolor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+				}
+				printf("%s\n",choice[i]);
+			}
+			Sleep(50);
+		}
+	}
+	return choose;
+}
+void downinit() {
+	printf("+---------------------------------------+\n|                                       |\n|  Paste your URL here,and PRESS ENTER  |\n|                                       |\n+---------------------------------------+\n");
 	URI.resize(1000);
 	scanf("%s",&URI[0]);
 }
-void down(){
-	strf=".\\aria2\\aria2c"+" --dir="+DIR+" --spilt="+int2str(connect)+" "+Personset;
-	strcpy("")
+void down() {
+	strf=strf+" --dir=download "+" -j 32 -s 32 -x 32 "+Personset+" "+URI;
+	strcpy(f,strf.c_str());
+	system(f);
 }
-void
-int main(){
-	init();
+void init() {
+	char tmp[100][MAXLEN]= {"CHOICEA","CHOICEB","C","d","aaaaaa","S**T"};
+	printf("%d",list("SIMPLETEST",tmp,6));
+}
+int main() {
+	downinit();
 }
 
